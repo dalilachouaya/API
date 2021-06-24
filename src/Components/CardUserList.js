c9e04fbed02b0de6b3510b980d31707ade6c233c
DALILA <aliladch2015@gmail.com>
2021-06-24 09:05:16 +0200

    second commit

--- src/Components/CardUserList.js
+++ src/Components/CardUserList.js
@@ -1,31 +1,50 @@
 
 import React from 'react';
 import Flippy, { FrontSide, BackSide } from 'react-flippy';
-import { makeStyles } from '@material-ui/core/styles';
-import Avatar from '@material-ui/core/Avatar';
-import { deepOrange } from '@material-ui/core/colors';
 import './CardUser.css';
+import { VscLocation} from "react-icons/vsc";
+import {AiOutlinePhone,AiOutlineMail} from "react-icons/ai";
+import {CgWebsite}  from "react-icons/cg";
+import { IconContext } from "react-icons";
 
 
-const useStyles = makeStyles((theme) => ({
-    orange: {
-        color: theme.palette.getContrastText(deepOrange[500]),
-        backgroundColor: deepOrange[500],
-            verticalAlign: 'middle',
-        
-    },}));
+
 
 const CardUserList = ({user}) => {
-    const classes = useStyles();
+
+
+                        /*********First method********/
+  /*
+  const initials =(fullName)=> {
+        // Create a array from fullName
+        let arrName = fullName.split(" ");
+  
+        // Select the first letter of the name
+        let iniName = fullName.charAt(0);
+  
+        // Select the first letter of the lastname
+        let iniLname = arrName[arrName.length - 1].charAt(0);
+  
+        // Return the initials
+        return iniName + iniLname;
+      } */
+
+                                    /*********** Second one */
+    
+    const initials =(fullName)=> {
+        const arrName = fullName.split(' ');
+        const initials = arrName.shift().charAt(0) + arrName.pop().charAt(0);
+        return initials.toUpperCase();
+    }
+
+                                          /**************** */
+
     return (
         <>
-        <Flippy
-        flipOnHover={true} 
-    flipDirection="horizontal" 
-    className='card'>
+        <Flippy  flipOnHover={true} flipDirection="horizontal" className='card' >
     <FrontSide >
-<div className="container">
-<Avatar alt="avatar" src="/broken-image.jpg" className={classes.orange}> {user.name[0]} </Avatar>
+        <div className="container">
+        <div className='avatar'>  { initials(user.name)} </div>
 
             <h3>{user.name}</h3>
             <h4>  {user.username}</h4>
@@ -38,11 +57,15 @@ const CardUserList = ({user}) => {
         </FrontSide>
         <BackSide >
             <div className="container">
-        <h4>email: {user.email} </h4>
-        <h4> address: {user.address.street}-{user.address.suite}-{user.address.city}-{user.address.zipcode}</h4>
-        <h5> {user.address.geo.lat}/{user.address.geo.lng}</h5>
-        <h4> phone: {user.phone} </h4>
-        <h4> website: {user.website} </h4>
+            <IconContext.Provider value={{ color: "#aab8c2",size:"1.6rem"}}>
+                <ul>
+            <li> <AiOutlineMail /> {user.email} </li>
+            <li><VscLocation/>  {user.address.street}-{user.address.suite}-{user.address.city}-{user.address.zipcode} </li>
+            <li>{user.address.geo.lat}/{user.address.geo.lng}</li>
+            <li><AiOutlinePhone/>{user.phone} </li>
+            <li>  <CgWebsite/>  {user.website}  </li>
+            </ul>
+            </IconContext.Provider>
             </div>
 
         </BackSide>
